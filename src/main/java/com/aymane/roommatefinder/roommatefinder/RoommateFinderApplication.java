7a036dabfbf1326d4db0e14581c09ed729485575
AymaneSghier <aymanesghier@gmail.com>
2025-09-16 19:44:08 -0400

    Initial commit with Spring Security config

---
+++ src/main/java/com/aymane/roommatefinder/roommatefinder/RoommateFinderApplication.java
@@ -0,0 +1,13 @@
+package com.aymane.roommatefinder.roommatefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoommateFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoommateFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aymane/roommatefinder/roommatefinder/RoommateFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aymane.roommatefinder.roommatefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoommateFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
