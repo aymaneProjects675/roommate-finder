c51986425cbec8b16dbbf265681bb65f743ff68c
aymaneProjects675 <aymaneprojects675@gmail.com>
2025-09-19 11:31:26 -0400

    Add complete authentication system with password hashing and login

--- src/main/java/com/aymane/roommatefinder/roommatefinder/controller/UserController.java
+++ src/main/java/com/aymane/roommatefinder/roommatefinder/controller/UserController.java
@@ -4,6 +4,10 @@ import com.aymane.roommatefinder.roommatefinder.model.User;
 import com.aymane.roommatefinder.roommatefinder.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/api")
@@ -11,6 +15,8 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
 
     @GetMapping("/users")
@@ -19,9 +25,20 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public String registerUser(@RequestBody User user){
+    public String registerUser(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         return "User registered successfully!";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User loginUser) {
+        Optional<User> user = userRepository.findByEmail(loginUser.getEmail());
+        if (user.isPresent() && passwordEncoder.matches(loginUser.getPassword(), user.get().getPassword())) {
+            return "Login successful!";
+        } else {
+            return "Invalid credentials!";
+        }
     }
 
 }
--- src/main/java/com/aymane/roommatefinder/roommatefinder/model/User.java
+++ src/main/java/com/aymane/roommatefinder/roommatefinder/model/User.java
@@ -1,8 +1,15 @@
 package com.aymane.roommatefinder.roommatefinder.model;
 
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "users")
 public class User {
 
