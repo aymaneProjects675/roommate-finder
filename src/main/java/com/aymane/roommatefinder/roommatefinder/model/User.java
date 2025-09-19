e23d2d086ee8f0bd7764ee95a94843ed2a9f2f82
aymaneProjects675 <aymaneprojects675@gmail.com>
2025-09-19 00:49:14 -0400

    Add User model with roommate preferences

---
+++ src/main/java/com/aymane/roommatefinder/roommatefinder/model/User.java
@@ -0,0 +1,28 @@
+package com.aymane.roommatefinder.roommatefinder.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+
+@Id
+private String id;
+private String email;
+private String password;
+private String firstName;
+private String lastName;
+
+private Integer minBudget;
+private Integer maxBudget;
+private String lifestyle;
+private Boolean smokingOk;
+private Boolean petsOk;
+
+private String bio;
+private Integer age;
+private String gender;
+
+private boolean active = true;
+
+}
