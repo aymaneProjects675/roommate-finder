5637a290670fc537c61b0665210f4b8859d7b68d
aymaneProjects675 <aymaneprojects675@gmail.com>
2025-10-02 14:56:30 -0400

    Add budget filtering logic to search endpoint

--- src/main/java/com/aymane/roommatefinder/roommatefinder/controller/RoommateController.java
+++ src/main/java/com/aymane/roommatefinder/roommatefinder/controller/RoommateController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.aymane.roommatefinder.roommatefinder.repository.UserRepository;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.util.ArrayList; 
 
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,11 +21,23 @@ public class RoommateController {
     private UserRepository userRepository;
 
     @GetMapping("/search")
-    public List<User> searchRoommates(
-            @RequestParam(required = false) Integer minBudget,
-            @RequestParam(required = false) Integer maxBudget
-    ) {
-        return userRepository.findAll();
+    public List<User> searchRoommates(@RequestParam(required = false) Integer minBudget, @RequestParam(required = false) Integer maxBudget) {
+        if (minBudget == null && maxBudget == null) {
+            return userRepository.findAll();
+        }
+
+        List<User> allUsers = userRepository.findAll();
+        List<User> matchingUsers = new ArrayList<>();
+
+        for (User user : allUsers) {
+            if (user.getMinBudget() != null && user.getMaxBudget() != null) {
+                if (user.getMinBudget() <= maxBudget && user.getMaxBudget() >= minBudget) {
+                    matchingUsers.add(user);
+                }
+            }
+        }
+
+        return matchingUsers;
     }
 
 
